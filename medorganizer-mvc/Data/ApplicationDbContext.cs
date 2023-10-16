7483d1dc442861a2f6a13b80c59f096389adbdfb
Easy Control <evandro.kumasaka@gmail.com>
2023-10-16 17:05:21 -0300

    Criação da tabela Medicamentos

--- medorganizer-mvc/Data/ApplicationDbContext.cs
+++ medorganizer-mvc/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using MedOrganizer.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace MedOrganizer.Data
@@ -9,5 +10,7 @@ namespace MedOrganizer.Data
             : base(options)
         {
         }
+        
+        public DbSet<Medicamento> Medicamentos { get; set; }
     }
 }
