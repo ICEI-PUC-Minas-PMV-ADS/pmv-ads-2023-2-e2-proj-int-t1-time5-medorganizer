0a14510064dc9c6ed86fb3e7f24c702541b60a38
Easy Control <evandro.kumasaka@gmail.com>
2023-12-06 23:25:27 -0300

    removendo a pasta antiga /medorganizer-mvc

--- medorganizer-mvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MedOrganizer.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
